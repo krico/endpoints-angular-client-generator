d5067c83d8d03d91afafb611f154a5269bc9b163
krico <github@cwa.to>
2015-09-17 23:15:41 +0200

    ng-module replaced by module-writer

--- test/module-writer.spec.js
+++ test/module-writer.spec.js
@@ -4,29 +4,21 @@ var assert = chai.assert;
 var expect = chai.expect;
 var streams = require('memory-streams');
 
-var NgModule = require(path.dirname(__filename) + '/../lib/ng-module');
+var ModuleWriter = require(path.dirname(__filename) + '/../lib/module-writer');
 
-describe('NgModule', function () {
+describe('ModuleWriter', function () {
 
     it('should write its canonical form to stream', function (done) {
         var out = new streams.WritableStream();
-        new NgModule('myMod').write(out);
+        new ModuleWriter().setName('myMod').write(out);
         expect(out.toString()).to.equal('angular.module(\'myMod\',[]);');
         done();
     });
 
     it('should write dependencies passed as string', function (done) {
         var out = new streams.WritableStream();
-        new NgModule('myMod', 'dep1').write(out);
+        new ModuleWriter().setName('myMod').addDependency('dep1').write(out);
         expect(out.toString()).to.equal('angular.module(\'myMod\',[\'dep1\']);');
         done();
     });
-
-    it('should write dependencies passed as array', function (done) {
-        var out = new streams.WritableStream();
-        new NgModule('myMod', ['dep1', 'dep2']).write(out);
-        expect(out.toString()).to.equal('angular.module(\'myMod\',[\'dep1\',\'dep2\']);');
-        done();
-    });
-
 });
