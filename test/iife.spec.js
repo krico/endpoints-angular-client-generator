f98f321068e056e180959b91a9e13f5b0d3b179a
krico <github@cwa.to>
2015-09-17 21:56:48 +0200

    iife no longer with constructor args

--- test/iife.spec.js
+++ test/iife.spec.js
@@ -10,22 +10,15 @@ describe('Iife', function () {
 
     it('should write its canonical form to stream', function (done) {
         var out = new streams.WritableStream();
-        new Iife().write(out);
-        expect(out.toString()).to.equal('(function () {})();');
+        new Iife().setStrict(false).write(out);
+        expect(out.toString()).to.equal('(function(){})();');
         done();
     });
 
     it('should support global variables as array when writing to stream', function (done) {
         var out = new streams.WritableStream();
-        new Iife([], ['a', 'b']).write(out);
-        expect(out.toString()).to.equal('(function (a,b) {})(a,b);');
-        done();
-    });
-
-    it('should support global variables as single when writing to stream', function (done) {
-        var out = new streams.WritableStream();
-        new Iife([], 'b').write(out);
-        expect(out.toString()).to.equal('(function (b) {})(b);');
+        new Iife().setStrict(false).addGlobal(['a', 'b']).write(out);
+        expect(out.toString()).to.equal('(function(a,b){})(a,b);');
         done();
     });
 
@@ -43,24 +36,22 @@ describe('Iife', function () {
                 out.write(s2);
             }
         };
-        new Iife([w1, w2]).write(out);
-        expect(out.toString()).to.equal('(function () {' + s1 + s2 + '})();');
+        new Iife().setStrict(false).append([w1, w2]).write(out);
+        expect(out.toString()).to.equal('(function(){' + s1 + s2 + '})();');
         done();
     });
 
-    it('should support writers as a single object when writing to stream', function (done) {
+    it('should use strict', function () {
         var out = new streams.WritableStream();
-        var s1 = '1;';
-        var w1 = {
-            write: function (out) {
-                out.write(s1);
-            }
-        };
-        new Iife(w1).write(out);
-        expect(out.toString()).to.equal('(function () {' + s1 + '})();');
-        done();
+        new Iife().setStrict(true).write(out);
+        expect(out.toString()).to.equal('(function(){\'use strict\';})();');
     });
 
+    it('should use strict with writers and globals', function () {
+        var out = new streams.WritableStream();
+        new Iife().setStrict(true).append('a;').addGlobal('g').write(out);
+        expect(out.toString()).to.equal('(function(g){\'use strict\';a;})(g);');
+    });
 
     it('should support different combinations on constructor', function (done) {
         var s1 = '1;';
@@ -76,20 +67,8 @@ describe('Iife', function () {
             }
         };
         var out = new streams.WritableStream();
-        new Iife([w1, w2], ['a', 'b']).write(out);
-        expect(out.toString()).to.equal('(function (a,b) {' + s1 + s2 + '})(a,b);');
-
-        out = new streams.WritableStream();
-        new Iife([w1, w2], 'b').write(out);
-        expect(out.toString()).to.equal('(function (b) {' + s1 + s2 + '})(b);');
-
-        out = new streams.WritableStream();
-        new Iife(w2, 'b').write(out);
-        expect(out.toString()).to.equal('(function (b) {' + s2 + '})(b);');
-
-        out = new streams.WritableStream();
-        new Iife(w2, ['b', 'c']).write(out);
-        expect(out.toString()).to.equal('(function (b,c) {' + s2 + '})(b,c);');
+        new Iife().setStrict(false).append([w1, w2]).setGlobals(['a', 'b']).write(out);
+        expect(out.toString()).to.equal('(function(a,b){' + s1 + s2 + '})(a,b);');
 
         done();
     });
