6118cf7aa4e104db27e89357c83dd481f1f4965a
krico <github@cwa.to>
2015-09-20 23:26:23 +0200

    Generated full fledged client for "APIs Discovery Service" https://developers.google.com/discovery/

--- example/app.js
+++ example/app.js
@@ -1,23 +1,64 @@
 (function (angular) {
     'use strict';
 
-    var example = angular.module('example', ['brain']);
+    var example = angular.module('example', ['ngSanitize', 'discovery']);
 
-    example.config(function (BrainProvider) {
-        BrainProvider.apiRoot('https://jasify-schedule.appspot.com/_ah/api/jasify/v1/');
+    example.config(function (DiscoveryProvider) {
+        //DiscoveryProvider.apiRoot('https://localhost:8080/_ah/api/jasify/v1/');
     });
 
     example.controller('AppController', AppController);
+    example.filter('prettyJSON', function () {
+        function syntaxHighlight(json) {
+            return JSON ? JSON.stringify(json, null, '  ') : 'your browser does not support JSON so cant pretty print';
+        }
 
-    function AppController(Brain) {
+        return syntaxHighlight;
+    });
+
+    function AppController($log, $location, $scope, Discovery) {
         var vm = this;
         vm.title = 'endpoints-angular-client-generator example';
-        vm.result = 'Calling Brain.apiInfo() ...';
+        vm.apisResponse = 'Calling Discovery.apis.list() ...';
+        vm.apis = [];
+        vm.selectedApi = null;
         vm.failure = null;
-        Brain.apiInfo().then(function (res) {
-            vm.result = res;
-        }, function (res) {
-            vm.failure = res;
+        vm.list = list;
+        vm.discoveryDocument = null;
+        vm.getDiscoveryDocument = getDiscoveryDocument;
+        vm.debug = $location.search().debug;
+        vm.preferred = true;
+        vm.name = undefined;
+        vm.list();
+
+        $scope.$watch('vm.selectedApi', function (newValue, oldValue) {
+            if (newValue !== oldValue) {
+                if (angular.isObject(newValue)) {
+                    vm.getDiscoveryDocument(newValue);
+                }
+            }
         });
+
+        function getDiscoveryDocument(api) {
+            vm.discoveryDocument = 'Discovery.apis.getRest(' + api.name + ',' + api.version + ');';
+            Discovery.apis.getRest(api.name, api.version).then(function (result) {
+                vm.discoveryDocument = result.data;
+            });
+        }
+
+        function list() {
+            $log.debug('Listing discovery APIs');
+            vm.selectedApi = null;
+            vm.discoveryDocument = null;
+            vm.apis = [];
+            Discovery.apis.list(vm.name, vm.preferred).then(function (res) {
+                vm.apisResponse = res;
+                vm.apis = res.data.items;
+                $log.debug('DISCOVERED: ' + vm.apis.length);
+            }, function (res) {
+                $log.warn('FAILED: ' + angular.toJson(res));
+                vm.failure = res;
+            });
+        }
     }
 })(angular);
--- test/generator.spec.js
+++ test/generator.spec.js
@@ -5,15 +5,29 @@ var expect = chai.expect;
 var streams = require('memory-streams');
 
 var DISCOVERY_FILE = path.dirname(__filename) + '/discovery.json';
+var DISCOVERY_DISCOVERY_FILE = path.dirname(__filename) + '/../doc/discovery-v1-discovery.json';
 
 var Generator = require(path.dirname(__filename) + '/../lib/generator');
+var Context = require(path.dirname(__filename) + '/../lib/context');
+var RestDescription = require(path.dirname(__filename) + '/../lib/rest-description');
 
 describe('Generator', function () {
+
+    afterEach(function () {
+        Context.restDescription(new RestDescription(DISCOVERY_FILE));
+    });
 
     it('should generate', function (done) {
         var out = new streams.WritableStream();
 
         Generator.generate(DISCOVERY_FILE, out, done);
+        //console.log(out.toString());
+    });
+
+    it('should generate discovery api', function (done) {
+        var out = new streams.WritableStream();
+
+        Generator.generate(DISCOVERY_DISCOVERY_FILE, out, done);
         console.log(out.toString());
     });
 });
